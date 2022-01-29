f3d24d96ef5e4a10b31c4dd79013c054f76190b5
Sarah Indano <sarah.indano@gmail.com>
2022-01-29 14:45:43 -0500

    commit tree 1278ab5fd1cfbee727ecc352e3d2e9086af23ac9

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solutions in this file
+
+//for (let age = 30; age < 40; age++) {
+//    console.log(`I'm ${age} years old. Happy birthday to me!`);
+//}
+
+//const gifts = ["teddy bear", "drone", "doll"]
+//
+//function wrapGifts(gifts) {
+  //  for (let i = 0; i < gifts.length; i++) {
+    //    console.log(`Wrapped ${gifts[i]} and added a bow!`)
+    //}
+    //return gifts
+//}
+
+//wrapGifts(gifts)
+
+const name = ["Guadalupe", "Ollie", "Aki"]
+const event = ["surprise"]
+const thankYouCards = []
+
+function writeCards(name, event) {
+    for (let i = 0; i < name.length; i++) {
+        thankYouCards.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+        debugger;
+    }
+return thankYouCards
+}
+
+function countDown() {
+let number = 10
+while (number >= 0) {
+    console.log(number--)
+}
+
+return countDown
+}
